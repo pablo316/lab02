902e174d23fa9fd580b858c5139d3f830ddf338e
pablo316 <lecter.whs@gmail.com>
2014-04-13 21:54:40 -0300

    prueba

---
+++ LAB/src/lab/Lab.java
@@ -0,0 +1,17 @@
+package lab;
+import javax.swing.JFrame;
+public class Lab {
+	
+		 public static void main(String[] arg) {
+			 
+			 JFrame frame = new JFrame();
+			 frame.setTitle("Mi primera ventana");
+			 frame.setSize(800, 600);
+			 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			 frame.setVisible(true);
+			 
+		 }
+
+	
+
+}
